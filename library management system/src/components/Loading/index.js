29d153d894a35982b562c7d5ee81c5f60103b976
swifts Lee <56683317+lcrazyl@users.noreply.github.com>
2022-05-22 15:54:55 +0800

    Delete library management system directory

--- library management system/src/components/Loading/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Spin } from 'antd';
-export default function index() {
-    return (
-        <Spin tip="Loading..." />
-    )
-}
--- library management system/src/utils/index.js
+++
@@ -1,6 +0,0 @@
-export const nav = {
-    "/": '全部图书',
-    "/borrower": '借书人',
-    "/bookTypeAnalysis": "图书分析",
-    "/userAnalysis": "用户分析"
-}
